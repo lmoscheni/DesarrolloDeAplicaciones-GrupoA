6b642ea11b7143ecc173d9d41650231aa9913053
skalic.julian <skalic.julian@gmail.com>
2014-12-07 04:09:18 -0300

    Import de operaciones listo, falta crearlas

--- desapp-grupoa-frontend/app/scripts/controllers/VerOperacionesCtrl.js
+++ desapp-grupoa-frontend/app/scripts/controllers/VerOperacionesCtrl.js
@@ -94,7 +94,35 @@ app.controller('VerOperacionesCtrl', function ($http,$scope,$location,$route,ngD
         return $scope.listPages;
     };
     
-    
+    $("#filename").change(function(e) {
+    var ext = $("input#filename").val().split(".").pop().toLowerCase();
+
+    if($.inArray(ext, ["csv"]) == -1) {
+        alert('Upload CSV');
+        return false;
+    }
+
+    if (e.target.files != undefined) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+        var csvval=e.target.result.split("\n");
+        var csvvalue=csvval[0].split(",");
+        var inputrad="";
+        for(var i=0;i<csvvalue.length;i++)
+        {
+            var temp=csvvalue[i];
+            var inputrad=inputrad+" "+temp;
+        }
+        $("#csvimporthint").html(inputrad);
+        $("#csvimporthinttitle").show();
+    };
+    reader.readAsText(e.target.files.item(0));
+
+    }
+
+    return false;
+
+    });
 });
 
 app.filter('offset', function() {
@@ -103,3 +131,4 @@ app.filter('offset', function() {
     return input.slice(start);
   };
 });
+
